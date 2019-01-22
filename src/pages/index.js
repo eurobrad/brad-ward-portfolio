cac567dc6efaf1e40a755cd563fd0e848cfa8372
Tyson Williams <tyson.j.williams@gmail.com>
2019-01-22 16:53:08 -0700

    Reformat SEO element and delete unused image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`brad ward`, `sound design`, `sound composition`, `composition for theatre`, `av`, `audio visual`, `theatrical systems designer`, `audio engineering`]} />
+    <SEO
+      title="Home"
+      keywords={[
+        `brad ward`,
+        `sound design`,
+        `sound composition`,
+        `composition for theatre`,
+        `av`,
+        `audio visual`,
+        `theatrical systems designer`,
+        `audio engineering`,
+      ]}
+    />
     
   </Layout>
 )
