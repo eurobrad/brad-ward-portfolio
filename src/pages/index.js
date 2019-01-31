758ebd949e58333f73c6fa541806fbe7814cee7d
Tyson Williams <tyson.j.williams@gmail.com>
2019-01-30 17:58:38 -0700

    Update main first section with 3 new images

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+import { BWAudioDesign } from '../components/images/image-bradward_audio-design'
+import { CtsdCertified } from '../components/images/image-ctsd'
+import { BWEducation } from '../components/images/image-bradward-education'
+
 const IndexPage = () => (
   <Layout>
     <SEO
@@ -18,7 +22,23 @@ const IndexPage = () => (
         `audio engineering`,
       ]}
     />
-    
+    <h2 style={{ marginBottom: `1.45rem`, textAlign: 'center', color: `rebeccapurple` }}>Sound Design and Composition for Theatre</h2>
+    <main>
+      <section>
+        <div style={{ width: `400px`, display: `inline-block`, padding: 10 }}>
+          <BWAudioDesign/>
+        </div>
+        <div style={{ width: `400px`, display: `inline-block`, padding: 10 }}>
+          <CtsdCertified/>
+        </div>
+        <div style={{ width: `400px`, display: `inline-block`, padding: 10 }}>
+          <BWEducation/>
+        </div>
+      </section>
+      <section style={{ margin: `4.35rem 0`, color: `rebeccapurple` }}>
+        <h2 style={{ textAlign: 'center' }}>Portfolio Matrix going here! :)</h2>
+      </section>
+    </main>
   </Layout>
 )
 
