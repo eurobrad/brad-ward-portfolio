6bcbfc3c0ad6668e7da1b7b0d0bb674402882a7f
Tyson Williams <tyson.j.williams@gmail.com>
2019-02-01 11:12:23 -0700

    Update main first section Add descriptions per image Update spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,14 +25,26 @@ const IndexPage = () => (
     <h2 style={{ marginBottom: `1.45rem`, textAlign: 'center', color: `rebeccapurple` }}>Sound Design and Composition for Theatre</h2>
     <main>
       <section>
-        <div style={{ width: `400px`, display: `inline-block`, padding: 10 }}>
+        <div style={{ width: `390px`, display: `inline-block`, padding: 10 }}>
           <BWAudioDesign/>
+          <h4 style={{ marginTop: `1.45rem`, textAlign: `center` }}>Sound Design and Composition</h4>
+          <p>
+            Brad is an award winning Sound Designer and Composer. Brad is also available for Projection/Media Design. He has also worked as a Project Manager for events worldwide. Brad also creates amazing custom Sound Effects and Soundscapes and is an expert in Show Control.
+          </p>
         </div>
-        <div style={{ width: `400px`, display: `inline-block`, padding: 10 }}>
+        <div style={{ width: `390px`, display: `inline-block`, padding: 10 }}>
           <CtsdCertified/>
+          <h4 style={{ marginTop: `1.45rem`, textAlign: `center` }}>AV and Theatrical Systems Designer</h4>
+          <p>
+            Brad is a NYC based CTS-D Certified design consultant working as Senior AV/Theatrical Systems Designer at DLR Group | Westlake Reed Leskosky. Brad can objectively design and engineer a system right for your application. Please contact Brad to discuss your next project.
+          </p>
         </div>
-        <div style={{ width: `400px`, display: `inline-block`, padding: 10 }}>
+        <div style={{ width: `390px`, display: `inline-block`, padding: 10 }}>
           <BWEducation/>
+          <h4 style={{ marginTop: `1.45rem`, textAlign: `center` }}>Education</h4>
+          <p>
+            Brad has provided successful workshops, classes, training, lectures, and demonstrations throughout the world. Brad is known for his clear communication at the level he is teaching which has ranged from primary and graduate students to industry professionals. Let Brad design a learning experience for you.
+          </p>
         </div>
       </section>
       <section style={{ margin: `4.35rem 0`, color: `rebeccapurple` }}>
