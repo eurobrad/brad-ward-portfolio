992500bd3da320e0a83133a5361449acb5fa691d
Tyson Williams <tyson.j.williams@gmail.com>
2019-04-10 17:40:35 -0600

    Update header Add hidden (make same color) bottom border for navs Make margin for nav more dynamic per view with media queries

--- src/components/header.js
+++ src/components/header.js
@@ -12,20 +12,39 @@ const HeaderMain = styled.header`
 
 const NavButtonWrapper = styled.div`
   display: inline-block;
+  @media (min-width: 611px) {
+    margin-left: 4em;
+  }
+  @media (min-width: 700px) {
+    margin-left: 6em;
+  }
+  @media (min-width: 800px) {
+    margin-left: 12em;
+  }
+  @media (min-width: 1000px) {
+    margin-left: 22em;
+  }
+  @media (min-width: 1199px) {
+    margin-left: 32em;
+  }
 `;
 
 const NavButton = styled.h2`
   display: inline-block;
-  margin: 10px;
+  margin: 0 10px;
+  border-bottom: 3px solid rebeccapurple;
   :hover {
     border-bottom: 3px solid white;
+  }
+  @media (max-width: 610px) {
+    margin-bottom: 20px;
   }
 `;
 
 const Header = ({ siteTitle }) => (
   <HeaderMain>
     <nav>
-      <h1 style={{ display: `inline-block`, marginRight: `1.45em` }}>
+      <h1 style={{ display: `inline-block` }}>
         <Link
           to="/"
           style={{
@@ -47,7 +66,7 @@ const Header = ({ siteTitle }) => (
             Home
           </Link>
       </NavButton>
-      <NavButton style={{ }}>
+      <NavButton>
           <Link
             to="/about"
             style={{
