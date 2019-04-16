453395e8be71a18e73b00b628dac5d2f5e504beb
Tyson Williams <tyson.j.williams@gmail.com>
2019-04-16 13:47:32 -0600

    Update About page Center title Add margin to sides to add space

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+const About = styled.div`
+  margin: 0 8px;
+  h1 {
+    text-align: center;
+  }
+`;
+
 const SecondPage = () => (
   <Layout>
     <SEO title="About" />
-    <h1>About Brad</h1>
-    <p>Brad Ward is a New York City-based Sound Designer. He works as a Senior AV and Theatrical Systems Designer at <a href="http://www.dlrgroup.com" rel="noopener" target="_blank">DLR Group | Westlake Reed Leskosky</a>.</p>
-    <p>With his expertise in theatrical sound design and the academe, Brad is available for commissioned projects, teaching, and workshops on a freelance basis.</p>
-    <p>Previously, Brad worked as Assistant Professor of Audio Engineering Technology at Belmont University in Nashville, Project Manager for Blitz Communications in London, and Lead Audio for Cirque du Soleil’s The Beatles LOVE in Las Vegas.</p>
-    <p>Brad serves on the education committee for the Theatrical Sound Designers and Composers Association, is a vice commissioner for sound for United States Institute for Theatre Technology, and is a member of the Association of Sound Designers in the UK. He was the curator for Sound Kitchen at World Stage Design 2017 in Taipei. Brad enjoys sharing his knowledge through lectures and adjunct teaching assignments.</p>
-    <p>Brad holds an MFA degree in Sound Design from Yale School of Drama.</p>
-    <p><strong>Download Resume:</strong>  —  <a href="http://bradward.net/resume/Resume%20-%20Brad%20Ward%20-%20Sound%20Design%20-%20Web.pdf" rel="noopener" target="_blank">Theatrical/Sound Design</a> — <a href="http://bradward.net/resume/Resume%20-%20Brad%20Ward%20-%20Consulting%20-%20Education%20-%20web.pdf" rel="noopener" target="_blank">Consulting/Education</a></p>
+    <About>
+      <h1>About Brad</h1>
+      <p>Brad Ward is a New York City-based Sound Designer. He works as a Senior AV and Theatrical Systems Designer at <a href="http://www.dlrgroup.com" rel="noopener" target="_blank">DLR Group | Westlake Reed Leskosky</a>.</p>
+      <p>With his expertise in theatrical sound design and the academe, Brad is available for commissioned projects, teaching, and workshops on a freelance basis.</p>
+      <p>Previously, Brad worked as Assistant Professor of Audio Engineering Technology at Belmont University in Nashville, Project Manager for Blitz Communications in London, and Lead Audio for Cirque du Soleil’s The Beatles LOVE in Las Vegas.</p>
+      <p>Brad serves on the education committee for the Theatrical Sound Designers and Composers Association, is a vice commissioner for sound for United States Institute for Theatre Technology, and is a member of the Association of Sound Designers in the UK. He was the curator for Sound Kitchen at World Stage Design 2017 in Taipei. Brad enjoys sharing his knowledge through lectures and adjunct teaching assignments.</p>
+      <p>Brad holds an MFA degree in Sound Design from Yale School of Drama.</p>
+      <p><strong>Download Resume:</strong>  —  <a href="http://bradward.net/resume/Resume%20-%20Brad%20Ward%20-%20Sound%20Design%20-%20Web.pdf" rel="noopener" target="_blank">Theatrical/Sound Design</a> — <a href="http://bradward.net/resume/Resume%20-%20Brad%20Ward%20-%20Consulting%20-%20Education%20-%20web.pdf" rel="noopener" target="_blank">Consulting/Education</a></p>
+    </About>
   </Layout>
 )
 
