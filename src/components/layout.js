1a5e77f9ad55a7724ecd450f9c240d9b40547274
Tyson Williams <tyson.j.williams@gmail.com>
2019-04-16 14:27:18 -0600

    Update Home page styling Fix spacing Make more fluid Change text to black color Make headings larger Alight contect sections to top Use styled-components

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,8 +23,7 @@ const Layout = ({ children }) => (
           style={{
             margin: `0 auto`,
             maxWidth: 1260,
-            padding: `0px 1.0875rem 1.45rem 0.5rem`,
-            paddingTop: 0,
+            padding: `8px`,
           }}
         >
           {children}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -6,6 +7,20 @@ import SEO from '../components/seo'
 import { BWAudioDesign } from '../components/images/image-bradward_audio-design'
 import { CtsdCertified } from '../components/images/image-ctsd'
 import { BWEducation } from '../components/images/image-bradward-education'
+
+const ContentSection = styled.div`
+  display: inline-block;
+  width: 33%;
+  margin: 0;
+  padding: 10px;
+  vertical-align: top;
+  @media (max-width: 1000px) {
+    width: 50%;
+  }
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+`;
 
 const IndexPage = () => (
   <Layout>
@@ -22,32 +37,32 @@ const IndexPage = () => (
         `audio engineering`,
       ]}
     />
-    <h2 style={{ marginBottom: `1.45rem`, textAlign: 'center', color: `rebeccapurple` }}>Sound Design and Composition for Theatre</h2>
+    <h1 style={{ marginBottom: `1.45rem`, textAlign: 'center' }}>Sound Design and Composition for Theatre</h1>
     <main>
       <section>
-        <div style={{ width: `390px`, display: `inline-block`, padding: 10 }}>
+        <ContentSection>
           <BWAudioDesign/>
-          <h4 style={{ marginTop: `1.45rem`, textAlign: `center` }}>Sound Design and Composition</h4>
+          <h3 style={{ marginTop: `1.45rem`, textAlign: `center` }}>Sound Design and Composition</h3>
           <p>
             Brad is an award winning Sound Designer and Composer. Brad is also available for Projection/Media Design. He has also worked as a Project Manager for events worldwide. Brad also creates amazing custom Sound Effects and Soundscapes and is an expert in Show Control.
           </p>
-        </div>
-        <div style={{ width: `390px`, display: `inline-block`, padding: 10 }}>
+        </ContentSection>
+        <ContentSection>
           <CtsdCertified/>
-          <h4 style={{ marginTop: `1.45rem`, textAlign: `center` }}>AV and Theatrical Systems Designer</h4>
+          <h3 style={{ marginTop: `1.45rem`, textAlign: `center` }}>AV and Theatrical Systems Designer</h3>
           <p>
             Brad is a NYC based CTS-D Certified design consultant working as Senior AV/Theatrical Systems Designer at DLR Group | Westlake Reed Leskosky. Brad can objectively design and engineer a system right for your application. Please contact Brad to discuss your next project.
           </p>
-        </div>
-        <div style={{ width: `390px`, display: `inline-block`, padding: 10 }}>
+        </ContentSection>
+        <ContentSection>
           <BWEducation/>
-          <h4 style={{ marginTop: `1.45rem`, textAlign: `center` }}>Education</h4>
+          <h3 style={{ marginTop: `1.45rem`, textAlign: `center` }}>Education</h3>
           <p>
             Brad has provided successful workshops, classes, training, lectures, and demonstrations throughout the world. Brad is known for his clear communication at the level he is teaching which has ranged from primary and graduate students to industry professionals. Let Brad design a learning experience for you.
           </p>
-        </div>
+        </ContentSection>
       </section>
-      <section style={{ margin: `4.35rem 0`, color: `rebeccapurple` }}>
+      <section style={{ margin: `4.35rem 0` }}>
         <h2 style={{ textAlign: 'center' }}>Portfolio Matrix going here! :)</h2>
       </section>
     </main>
