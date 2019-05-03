f4223bb971e7caa939ff5a3db4ce4c346820e1d6
Tyson Williams <tyson.j.williams@gmail.com>
2019-05-03 17:29:07 -0600

    Update portfolio matrix Add more portfolio pieces Update font sizes for smaller screen sizes

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,6 +37,9 @@ const PortfolioDescriptionTitle = styled.h4`
   @media (max-width: 500px) {
     font-size: 16px;
   }
+  @media (max-width: 375px) {
+    font-size: 12px;
+  }
 `
 
 const PortfolioDescriptionText = styled.p`
@@ -45,6 +48,9 @@ const PortfolioDescriptionText = styled.p`
   font-size: 12px;
   @media (max-width: 500px) {
     font-size: 10px;
+  }
+  @media (max-width: 375px) {
+    font-size: 8px;
   }
 `
 
@@ -255,6 +261,7 @@ const IndexPage = () => (
             </PortfolioDescription>
           </PortfolioPiece>
         </Link>
+        <Link to="/anEnemyOfThePeople">
         <PortfolioPiece>
           <PortfolioImage
             src={
@@ -270,6 +277,8 @@ const IndexPage = () => (
             </PortfolioDescriptionText>
           </PortfolioDescription>
         </PortfolioPiece>
+        </Link>
+        <Link to="/numberTheStars">
         <PortfolioPiece>
           <PortfolioImage
             src={
@@ -285,6 +294,143 @@ const IndexPage = () => (
             </PortfolioDescriptionText>
           </PortfolioDescription>
         </PortfolioPiece>
+        </Link>
+        <Link to="/theBeatlesLove">
+          <PortfolioPiece>
+            <PortfolioImage
+              src={
+                'http://bradward.net/wordpress/wp-content/uploads/2018/02/LoveLogo5.jpg'
+              }
+            />
+            <PortfolioDescription>
+              <PortfolioDescriptionTitle>
+                The Beatles LOVE
+              </PortfolioDescriptionTitle>
+              <PortfolioDescriptionText>
+                Cirque du Soleil - 2006-2008
+              </PortfolioDescriptionText>
+            </PortfolioDescription>
+          </PortfolioPiece>
+        </Link>
+        <Link to="/2012OlympicsLondon">
+          <PortfolioPiece>
+            <PortfolioImage
+              src={
+                'http://bradward.net/wordpress/wp-content/uploads/2018/02/olympic-rings.jpg'
+              }
+            />
+            <PortfolioDescription>
+              <PortfolioDescriptionTitle>
+                2012 OLYMPICS – LONDON
+              </PortfolioDescriptionTitle>
+              <PortfolioDescriptionText>
+                Sound Project Manager
+              </PortfolioDescriptionText>
+            </PortfolioDescription>
+          </PortfolioPiece>
+        </Link>
+        <Link to="/theBrooklynGaze">
+          <PortfolioPiece>
+            <PortfolioImage
+              src={
+                'http://bradward.net/wordpress/wp-content/uploads/2018/02/8big.jpg'
+              }
+            />
+            <PortfolioDescription>
+              <PortfolioDescriptionTitle>
+                Transformation &amp; Revelation
+              </PortfolioDescriptionTitle>
+              <PortfolioDescriptionText>
+                UK Design for Performance - 2011
+              </PortfolioDescriptionText>
+            </PortfolioDescription>
+          </PortfolioPiece>
+        </Link>
+        <Link to="/introToProductionDesign">
+          <PortfolioPiece>
+            <PortfolioImage
+              src={
+                'http://bradward.net/wordpress/wp-content/uploads/2018/02/Belmont-PJ_new.jpg'
+              }
+            />
+            <PortfolioDescription>
+              <PortfolioDescriptionTitle>
+                Intro to Production Design
+              </PortfolioDescriptionTitle>
+              <PortfolioDescriptionText>
+                Assistant Professor, Belmont University
+              </PortfolioDescriptionText>
+            </PortfolioDescription>
+          </PortfolioPiece>
+        </Link>
+        <Link to="/lesMisérables">
+          <PortfolioPiece>
+            <PortfolioImage
+              src={
+                'http://bradward.net/wordpress/wp-content/uploads/2018/02/Les-Miserables.jpg'
+              }
+            />
+            <PortfolioDescription>
+              <PortfolioDescriptionTitle>
+                Les Misérables
+              </PortfolioDescriptionTitle>
+              <PortfolioDescriptionText>
+                Belmont University - 2013
+              </PortfolioDescriptionText>
+            </PortfolioDescription>
+          </PortfolioPiece>
+        </Link>
+        <Link to="/prideAndPrejudice">
+          <PortfolioPiece>
+            <PortfolioImage
+              src={
+                'http://bradward.net/wordpress/wp-content/uploads/2018/02/Pride-Prejudice5.jpg'
+              }
+            />
+            <PortfolioDescription>
+              <PortfolioDescriptionTitle>
+                Pride and Prejudice
+              </PortfolioDescriptionTitle>
+              <PortfolioDescriptionText>
+                Bristol Riverside Theatre - 2013
+              </PortfolioDescriptionText>
+            </PortfolioDescription>
+          </PortfolioPiece>
+        </Link>
+        <Link to="/barnesFoundation">
+          <PortfolioPiece>
+            <PortfolioImage
+              src={
+                'http://bradward.net/wordpress/wp-content/uploads/2018/02/Barnes2.jpg'
+              }
+            />
+            <PortfolioDescription>
+              <PortfolioDescriptionTitle>
+                Barnes Foundation
+              </PortfolioDescriptionTitle>
+              <PortfolioDescriptionText>
+                Audiovisual System Design
+              </PortfolioDescriptionText>
+            </PortfolioDescription>
+          </PortfolioPiece>
+        </Link>
+        <Link to="/theMethuselahTree">
+          <PortfolioPiece>
+            <PortfolioImage
+              src={
+                'http://bradward.net/wordpress/wp-content/uploads/2018/02/Methusalah-Tree30Ed-copy.jpg'
+              }
+            />
+            <PortfolioDescription>
+              <PortfolioDescriptionTitle>
+                The Methuselah Tree
+              </PortfolioDescriptionTitle>
+              <PortfolioDescriptionText>
+                Cockroach Theatre - 2007
+              </PortfolioDescriptionText>
+            </PortfolioDescription>
+          </PortfolioPiece>
+        </Link>
       </Portfolio>
     </main>
   </Layout>
