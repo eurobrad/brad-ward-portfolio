d535171932ebd7bb312e5abc617335f89a0f5fb7
Tyson Williams <tyson.j.williams@gmail.com>
2019-05-03 12:42:02 -0600

    Update Time Stands Still page
    Add description
    Add View Project button
    Update spacing

--- src/pages/timeStandsStill.js
+++ src/pages/timeStandsStill.js
@@ -9,8 +9,23 @@ const PortfolioSubtitle = styled.p`
   color: #777;
 `
 
+const ProjectDescription = styled.section`
+  margin-top: 32px;
+  text-align: left;
+`
+
+const ViewProjectButton = styled.button`
+  background-color: rebeccapurple;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  box-shadow: 2px 2px 2px 2px #bd92e8;
+  cursor: pointer;
+`
+
 const PortfolioContent = styled.div`
   text-align: center;
+  padding: 8px;
 `
 
 const TimeStandsStill = () => (
@@ -35,6 +50,32 @@ const TimeStandsStill = () => (
         />
       </div>
       <script src="https://player.vimeo.com/api/player.js" />
+      <ProjectDescription>
+        <p>
+          In TIME STANDS STILL, we explore how the impact of war and terrorism
+          can change lives forever. In this case, a car bomb has severely
+          injured Sarah, and she returns home to recuperate in the Brooklyn loft
+          she shares with her boyfriend, James who has also had a similar
+          experience. Both characters struggle with physical and mental recovery
+          from their trauma, and how their experiences have changed their
+          relationship and each other. Can you really get back to “normal” or
+          does normal change?
+        </p>
+        <p>
+          Through music and sounds, we were able to help tell this story and
+          relate it to the bombs that sadly go off in our lives and the lives of
+          those we love. As you listen to this sampling of music and sound,
+          consider these questions: What do we value in our lives? Are we
+          addicted to chaos and war? How can our places in the world change? How
+          do we react to the terror? What does it mean to pick up and move
+          forward? Should we reject the comfortable life and to try to make a
+          difference for others? What’s the point?
+        </p>
+        <p>Listen…</p>
+      </ProjectDescription>
+      <a href="https://www.brtstage.org/2017-2018/time-stands-still/">
+        <ViewProjectButton>View Project</ViewProjectButton>
+      </a>
     </PortfolioContent>
   </Layout>
 )
