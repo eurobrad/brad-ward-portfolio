4b07b1e99a58dcefe3322b089fe6a9ef95e5677e
eurobrad <50499945+eurobrad@users.noreply.github.com>
2019-05-11 21:23:35 -0400

    resize youtube???

--- src/pages/jammer.js
+++ src/pages/jammer.js
@@ -75,8 +75,6 @@ const jammer = () => (
           //     allow="autoplay; fullscreen"
           //     allowfullscreen
 
-          width="560"
-          height="315"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
