dc5283829a7cdde834a3fc84b63b7d4ff7403b99
Tyson Williams <tyson.j.williams@gmail.com>
2019-05-13 16:11:54 -0600

    Update contact button

--- src/components/header.js
+++ src/components/header.js
@@ -87,7 +87,7 @@ const Header = ({ siteTitle }) => (
           <StyledLink to="/about">About</StyledLink>
         </NavButton>
         <NavButton>
-          <a href="mailto:info@bradward.net" target="_blank" rel="noopener">Contact</a>
+          <a href="mailto:info@bradward.design" target="_blank" rel="noopener">Contact</a>
         </NavButton>
       </NavWrapper>
     </nav>
