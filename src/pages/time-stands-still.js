88d9c327680e3cd4be98a8ea422e12fce2a799ee
Tyson Williams <tyson.j.williams@gmail.com>
2019-05-31 18:41:30 -0600

    Update Time Stands Still page content flow

--- src/pages/time-stands-still.js
+++ src/pages/time-stands-still.js
@@ -1,23 +1,29 @@
-import { Link } from 'gatsby'
 import React from 'react'
 import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const ProjectContent__Title = styled.h1``
+const ProjectContentTitle = styled.h1`
+  margin: 0 10px 10px 10px;
+  @media (min-width: 450px) {
+    margin-top: 26px;
+  }
+`
 
 const ProjectSubtitle = styled.p`
   color: #777;
+  margin: 10px;
+  @media (min-width: 450px) {
+    margin-bottom: 34px;
+  }
 `
 
 const ProjectMedia = styled.div`
   position: relative;
-  float: left;
-  clear: both;
-  width: 60%;
+  display: inline-block;
+  width: 49%;
   padding: 25% 0 0 0;
-  margin-right: 2%;
   iframe {
     position: absolute;
     top: 0;
@@ -28,14 +34,26 @@ const ProjectMedia = styled.div`
 `
 
 const ProjectDescription = styled.section`
-  margin-top: 32px;
+  display: inline-block;
+  width: 47%;
+  height: 465px;
+  overflow: auto;
+  vertical-align: top;
   text-align: left;
+  @media (max-width: 480px) {
+    width: 100%;
+    height: auto;
+  }
+  @media (min-width: 481px) {
+    margin-left: 16px;
+  }
 `
 
 const ViewProjectButton = styled.button`
   background-color: rebeccapurple;
   color: white;
   border: none;
+  margin-top: 96px;
   padding: 8px 16px;
   box-shadow: 2px 2px 2px 2px #bd92e8;
   border-radius: 5px;
@@ -46,7 +64,7 @@ const ProjectContent = styled.div`
   text-align: center;
   padding: 8px;
   @media (max-width: 480px) {
-    ${ProjectContent__Title} {
+    ${ProjectContentTitle} {
       font-size: 24px;
     }
     ${ProjectMedia} {
@@ -56,6 +74,12 @@ const ProjectContent = styled.div`
         width: 100%;
       }
     }
+    ${ViewProjectButton} {
+      margin-top: 168px;
+    }
+    ${ProjectDescription} {
+      margin-top: 32px;
+    }
   }
 `
 
@@ -63,7 +87,7 @@ const TimeStandsStill = () => (
   <Layout>
     <SEO title="Time Stands Still" />
     <ProjectContent>
-      <ProjectContent__Title>Time Stands Still</ProjectContent__Title>
+      <ProjectContentTitle>Time Stands Still</ProjectContentTitle>
       <ProjectSubtitle>Bristol Riverside Theatre - 2018</ProjectSubtitle>
       <ProjectMedia>
         <iframe
@@ -72,6 +96,9 @@ const TimeStandsStill = () => (
           allow="autoplay; fullscreen"
           allowfullscreen
         />
+        <a href="https://www.brtstage.org/2017-2018/time-stands-still/">
+          <ViewProjectButton>View Project</ViewProjectButton>
+        </a>
       </ProjectMedia>
       <script src="https://player.vimeo.com/api/player.js" />
       <ProjectDescription>
@@ -97,9 +124,6 @@ const TimeStandsStill = () => (
         </p>
         <p>Listen…</p>
       </ProjectDescription>
-      <a href="https://www.brtstage.org/2017-2018/time-stands-still/">
-        <ViewProjectButton>View Project</ViewProjectButton>
-      </a>
     </ProjectContent>
   </Layout>
 )
