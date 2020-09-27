d47e1895524057092961a2c948bb372f5e60644e
eurobrad <50499945+eurobrad@users.noreply.github.com>
2020-09-27 14:52:47 -0400

    Att Rose Tattoo Section and Assets

---
+++ src/pages/rose-tattoo.js
@@ -0,0 +1,119 @@
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import styled from 'styled-components'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const ProjectContentTitle = styled.h1`
+  margin: 0 10px 10px 10px;
+  @media (min-width: 450px) {
+    margin-top: 26px;
+  }
+`
+
+const ProjectSubtitle = styled.p`
+  color: #777;
+  margin: 10px;
+  @media (min-width: 450px) {
+    margin-bottom: 34px;
+  }
+`
+
+const ProjectMedia = styled.div`
+  display: inline-block;
+  width: 49%;
+  margin-right: 2%;
+`
+
+const ProjectDescription = styled.section`
+  display: inline-block;
+  width: 49%;
+  height: 465px;
+  overflow: auto;
+  vertical-align: top;
+  text-align: left;
+  @media (max-width: 480px) {
+    width: 100%;
+    height: auto;
+  }
+`
+
+const ViewProjectButton = styled.button`
+  position: relative;
+  z-index: 999;
+  background-color: rebeccapurple;
+  color: white;
+  border: none;
+  margin: 16px;
+  padding: 8px 16px;
+  box-shadow: 2px 2px 2px 2px #bd92e8;
+  border-radius: 5px;
+  cursor: pointer;
+`
+
+const ProjectContent = styled.div`
+  text-align: center;
+  padding: 8px;
+  @media (max-width: 480px) {
+    ${ProjectContentTitle} {
+      font-size: 24px;
+    }
+    ${ProjectDescription} {
+      margin-top: 32px;
+    }
+    ${ProjectMedia} {
+      width: 100%;
+      height: 250px;
+      .thumbs-wrapper {
+        display: none;
+      }
+    }
+  }
+`
+
+const RoseTattoo = () => (
+  <Layout>
+    <SEO title="The Rose Tattoo on Broadway" />
+    <ProjectContent>
+      <ProjectContentTitle>The Rose Tattoo on Broadway</ProjectContentTitle>
+      <ProjectSubtitle>Associate Sound Designer</ProjectSubtitle>
+      <ProjectMedia>
+        <Carousel autoPlay>
+          <div>
+            <img src="https://raw.githubusercontent.com/eurobrad/brad-ward-portfolio/master/src/images/rosetattoo/RedCarpet.jpg" />
+            <p className="legend">
+              Fitz Patton, Designer with Associates Sunny Kil & Brad Ward
+            </p>
+          </div>
+          <div>
+            <img src="https://raw.githubusercontent.com/eurobrad/brad-ward-portfolio/master/src/images/rosetattoo/brad-techtable.jpg" />
+            <p className="legend">Brad at Tech Tech Table</p>
+          </div>
+          <div>
+            <img src="https://raw.githubusercontent.com/eurobrad/brad-ward-portfolio/master/src/images/rosetattoo/frontshot.JPG" />
+            <p className="legend">The Rig</p>
+          </div>
+          <div>
+            <img src="https://raw.githubusercontent.com/eurobrad/brad-ward-portfolio/master/src/images/rosetattoo/speakers-sideshot.jpg" />
+            <p className="legend">Side Shot</p>
+          </div>
+        </Carousel>
+      </ProjectMedia>
+      <ProjectDescription>
+        <p>
+          Once again working wtih designer Fitz Patton and Associate Sunny Kil,
+          I was fortunate to be a part of the design team for this excellent
+          performance at the American Airlines Theatre. Directed by Trip Cullman
+          and staring Marisa Tomei. This wasn my first experience with TiMax
+          which proved to make the sound natural. I don't know how we would have
+          achieved this given the challenges of the cast's movement through the
+          space.
+        </p>
+      </ProjectDescription>
+    </ProjectContent>
+  </Layout>
+)
+
+export default RoseTattoo
