cec7ed70715be2820b23c508168dafd0dd9bd00a
eurobrad <50499945+eurobrad@users.noreply.github.com>
2020-10-03 10:30:40 -0400

    Update rose-tattoo.js
    brad brad bard

--- src/pages/rose-tattoo.js
+++ src/pages/rose-tattoo.js
@@ -133,7 +133,7 @@ const RoseTattoo = () => (
           and staring Marisa Tomei. This wasn my first experience with TiMax
           which proved to make the sound natural. I don't know how we would have
           achieved this given the challenges of the cast's movement through the
-          space. This is teh new stuff to add...... Luda Luda Luda
+          space. This is teh new stuff to add...... Luda Luda Luda Brad Brad Brad.
         </p>
       </ProjectDescription>
     </ProjectContent>
